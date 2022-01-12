bd3720a4cf6438939732633a264e6d097829fbbe
devon-n <devnathan94@gmail.com>
2022-01-12 11:35:38 +1100

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,26 @@
+import React from 'react'
 import logo from './logo.svg';
 import './App.css';
+
+import Home from './Home'
+import Contact from './Contact'
+import Projects from './Projects'
+import About from './About'
+
+import {Route, Routes, Link, Switch} from 'react-router-dom'
+import NavBar from './Navbar'
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NavBar />
+      <Routes>
+        <Route exact path ='/' component={Home} />
+        <Route exact path ='/contact' component={Contact} />
+        <Route exact path ='/projects' component={Projects} />
+        <Route exact path ='/about' component={About} />
+      </Routes>
+      
     </div>
   );
 }
